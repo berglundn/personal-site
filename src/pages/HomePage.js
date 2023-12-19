628f7ac5db875fa04a037dae5fce0330e3ddc7c6
berglundn <nathanjberglund@gmail.com>
2023-12-18 18:57:32 -0600

    Project foundation created

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+
+function HomePage(){
+    return(
+        <div>
+            <h1>
+                Hello and welcome to my website
+            </h1>
+            <p>
+                Unfortunately it is under construction at the moment.
+                Planned features include an archive of school and personal projects,
+                as well as work history and interests.
+            </p>
+        </div>
+    )
+}
+
+export default HomePage();
